f4517a220218be928de6bf4eb73a958f43e6e4f2
KennyAck001 <kennyack001@gmail.com>
2025-06-06 15:40:28 +0530

    My Personal Portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: ["./index.html", "./index.js"],
+  content: ["./index.html", "./index.js"],
   theme: {
-    
     extend: {
-      colors:{
-      customGreen: "#20C997",
-      lightGrey: "#343A40",
-      darkGrey: "#212529",
-      blueGrey:"#252B33",
-      hov:"#ffff5",
-    },
+      colors: {
+        customGreen: "#20C997",
+        lightGrey: "#343A40",
+        darkGrey: "#212529",
+        blueGrey: "#252B33",
+        backGround: "#00000080",
+      },
+      screens: {
+        'xs': '480px',  // Custom breakpoint for small screens
+        'lg': '1200px',
+        'cn':'1000px'// Custom breakpoint for large screens
+      },
     },
   },
   plugins: [],
 }
-
