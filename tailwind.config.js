e0ead82e0c72823fbeb88bdab1d00755f4f984a1
KennyAck001 <kennyack001@gmail.com>
2025-06-06 13:31:23 +0530

    My Personal Portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+content: ["./index.html", "./index.js"],
   theme: {
     
     extend: {
@@ -9,15 +9,8 @@ module.exports = {
       lightGrey: "#343A40",
       darkGrey: "#212529",
       blueGrey:"#252B33",
-      backGround:"#00000080",
+      hov:"#ffff5",
     },
-
-    screens:{
-        'xs': '475px',      // Extra small devices
-        'sm': '640px',      // Small devices (default)
-        'md': '768px',      // Medium devices (default)
-        'lg': '1110px',     //Large devices
-    }
     },
   },
   plugins: [],
